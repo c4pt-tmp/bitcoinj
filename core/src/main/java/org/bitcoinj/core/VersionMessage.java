ded28a3b2190e0a9106abbb531cef192e9527c19
c4pt000 <46433702+c4pt000@users.noreply.github.com>
2021-09-27 15:49:22 -0400

    Update VersionMessage.java

--- core/src/main/java/org/bitcoinj/core/VersionMessage.java
+++ core/src/main/java/org/bitcoinj/core/VersionMessage.java
@@ -43,10 +43,10 @@ import java.util.Locale;
  */
 public class VersionMessage extends Message {
 
-    /** The version of this library release, as a string. */
-    public static final String BITCOINJ_VERSION = "0.15.8.bisq.13";
+     /** The version of this library release, as a string. */
+    public static final String BITCOINJ_VERSION = "radioX-6.0.0";
     /** The value that is prepended to the subVer field of this application. */
-    public static final String LIBRARY_SUBVER = "/bitcoinj:" + BITCOINJ_VERSION + "/";
+    public static final String LIBRARY_SUBVER = "/Shibetoshi-RadioX:" + BITCOINJ_VERSION + "/";
 
     /** A service bit that denotes whether the peer has a full copy of the block chain or not. */
     public static final int NODE_NETWORK = 1 << 0;
