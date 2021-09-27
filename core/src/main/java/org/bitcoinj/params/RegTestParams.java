186536a4adb7f3cab9ebd5a4fae69d71700272d5
c4pt000 <46433702+c4pt000@users.noreply.github.com>
2021-09-27 18:44:30 -0400

    Update RegTestParams.java

--- core/src/main/java/org/bitcoinj/params/RegTestParams.java
+++ core/src/main/java/org/bitcoinj/params/RegTestParams.java
@@ -79,7 +79,6 @@ public class RegTestParams extends AbstractBitcoinNetParams {
                 genesis.setNonce(2);
                 genesis.setDifficultyTarget(0x207fFFFFL);
                 genesis.setTime(1296688602L);
-                checkState(genesis.getHashAsString().toLowerCase().equals("1b38af7fac04373a2619b6f0e8f2fc73f45380fb98bef338b41fb64e893b9cd2"));
             }
             return genesis;
         }
